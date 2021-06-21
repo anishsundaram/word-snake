7e80fd5262ec5e8b58ea300be23a69562d71cd83
Anish Sundaram <anishsun@umich.edu>
2021-06-20 23:22:14 -0700

    Add comments

--- word_snake.cpp
+++ word_snake.cpp
@@ -1,9 +1,15 @@
+// Anish Sundaram / anishsun
+// Word Snake Problem
+
+
+
 #include <vector>
 #include <iostream>
 #include <string>
 
 using namespace std;
 
+//Function to print matrix to command line
 void print_matrix(vector<vector<char> >& letter_matrix){
     for(vector<char>& vec : letter_matrix){
         for(char c : vec){
@@ -16,28 +22,37 @@ void print_matrix(vector<vector<char> >& letter_matrix){
 void word_snake(vector<string> word_list, vector<vector<char> >& letter_matrix){
     int vert_dist=0;
     int horiz_dist =0;
+	//Randomly chooses whether snake goes left or right
     int counter = rand()%2;
+	//Loop over words in word list vector
     for(uint i = 0; i < word_list.size(); ++i){
+		//Alternates between horizontal or vertical
         if(i%2==0){
+			//Alternates randomly for left-right snaking, this loop is for left
             if(counter==1 && (horiz_dist-int(word_list[i].size()))>0){
+				//Loops over all characters in word and adjusts index 
                 for(int start = 0; start< word_list[i].size(); ++start){
                     letter_matrix[vert_dist][horiz_dist] = word_list[i][start];
                     --horiz_dist;
                  }
+				//Adjust indices and re-randomize counter
                 vert_dist++;
                 counter = rand()%2;
                 ++horiz_dist;
             }
             else{
+				//For right-snaking loop
                 for(int start = 0; start< word_list[i].size(); ++start){
                     letter_matrix[vert_dist][horiz_dist] = word_list[i][start];
                     horiz_dist++;
                 }
+				//adjust for indices and randomize counter var
                 horiz_dist--;
                 vert_dist++;
                 counter = rand()%2;
             }
         }        
+		//For downward snaking
         else{
            for(uint start = 1; start < word_list[i].size(); ++start){
                     letter_matrix[vert_dist][horiz_dist] = word_list[i][start];
@@ -46,9 +61,11 @@ void word_snake(vector<string> word_list, vector<vector<char> >& letter_matrix){
             --vert_dist;
         }
     }
+	//Call print function and terminate
     print_matrix(letter_matrix);
 }
-
+// Function to read input from command line and input into a vector
+// Also designates size value for 2D field of chars
 uint parse_date(vector<string>& word_list){
     string temp = "";
     uint size = 0;
@@ -60,14 +77,16 @@ uint parse_date(vector<string>& word_list){
 }
 
 
-
+//driver code
 int main(int argc, char *argv[]){
     //Include for Xcode and replace * with (1-4) inclusive
-    //freopen("input2.txt", "r", stdin);
+    //freopen("input1.txt", "r", stdin);
     //freopen("output.txt", "w", stdout);
     
     vector<string> word_list;
     uint size = parse_date(word_list);
+	
+	// Initializes 2D Vector of chars to be filled with word snake
     vector<vector<char> > letter_matrix(size, vector<char>(size,' '));
     word_snake(word_list, letter_matrix);
 
